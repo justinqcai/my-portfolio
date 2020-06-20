642cfe749245f7ee2233c520ed58b281d62a57f6
Justin Cai <justincai1990@gmail.com>
2020-06-20 04:00:43 -0400

    Updates

---
+++ precache-manifest.5fc3568a42cc80c063008d9425d6c24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc2d079f6491d57572e92b2cd7dfe47",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d16d4f37c6f1ea387579",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8519b10dcff8b0b9288c",
+    "url": "/my-portfolio/static/js/2.14ceb798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.14ceb798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16d4f37c6f1ea387579",
+    "url": "/my-portfolio/static/js/main.52adac61.chunk.js"
+  },
+  {
+    "revision": "170f45050b91094c3a2e",
+    "url": "/my-portfolio/static/js/runtime-main.354f34ae.js"
+  }
+]);
